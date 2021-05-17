3c14d5c4893c4e456baa49c17661aa6db43a71c3
Jahor Laryn <mrojahor@gmail.com>
2021-05-17 09:46:29 +0300

    Add transition in router

--- frontend-vue/src/App.vue
+++ frontend-vue/src/App.vue
@@ -1,17 +1,28 @@
 <template>
   <v-app>
     <v-main>
-      <Bar/>
-      <router-view/>
+      <Bar />
+      <transition name="fade">
+        <router-view class="view" />
+      </transition>
     </v-main>
   </v-app>
 </template>
 
 <script>
-import Bar from './components/Bar'
-  export default {
-    components: {
-      Bar
-    }
-  }
+import Bar from "./components/Bar";
+export default {
+  components: {
+    Bar,
+  },
+};
 </script>
+
+<style>
+.fade-enter-active, .fade-leave-active {
+  transition: opacity .4s;
+}
+.fade-enter, .fade-leave-to {
+  opacity: 0;
+}
+</style>
